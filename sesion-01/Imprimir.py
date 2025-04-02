4f306d1eec80166297f39a3da23993ebd890f217
KABVez <kevinandreo56@gmail.com>
2025-04-01 20:19:23 -0500

    novo

---
+++ sesion-01/Imprimir.py
@@ -0,0 +1,6 @@
+#Este es un comentario de python, el interprete me ignora
+#veremos el uso de la funcion print
+print("hola mundo")
+print("el resultado de 5+3 es:", 5+3)
+nombre="Luis"
+print("Bienvenido {nombre} a nuestro skill")
