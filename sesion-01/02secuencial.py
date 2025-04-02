89b6d0fe8e21983566d47fa40bfb1332cb6fc536
KABVez <kevinandreo56@gmail.com>
2025-04-01 20:51:27 -0500

    ad

--- sesion-01/02secuencial.py
+++ sesion-01/02secuencial.py
@@ -3,15 +3,15 @@
 #el programa calcula el area de un triangulo
 
 #Entrada de datos
-    #pedir los datos para calcular el area del triangulo
-    base=float(input("Ingresa la base del triangulo"))
-    altura=float(input("Ingresa la altura del triangulo"))
+#pedir los datos para calcular el area del triangulo
+base=float(input("Ingresa la base del triangulo"))
+altura=float(input("Ingresa la altura del triangulo"))
 #procesamiento de datos
-    #ejecutar la formula para calcular el area del triangulo
-    area=(base*altura)/2
+#ejecutar la formula para calcular el area del triangulo
+area=(base*altura)/2
 #salida de datos
-    #mostrar la informacion (el area del trinagulo al usuario)
-        #V1
-        print("El area del triangulo es:",area)
-        #V2
-        print(f"El area del triangulo es:{area}")
+#mostrar la informacion (el area del trinagulo al usuario)
+#V1
+print("El area del triangulo es:",area)
+#V2
+print(f"El area del triangulo es:{area}")
