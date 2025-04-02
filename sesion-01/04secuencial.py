46218061b3fe1aa79ca7b325639bd41c866a7128
KABVez <kevinandreo56@gmail.com>
2025-04-01 21:22:45 -0500

    ad

--- sesion-01/04secuencial.py
+++ sesion-01/04secuencial.py
@@ -1,3 +1,6 @@
+#Libreria de python para usar tiempo
+import time
+
 #calcularemos el promedio de 3 calificaciones,
 #en mi caso tratare de indicar cuantos alumnos mas hay
 alumno=input("Alumno: ")
@@ -8,6 +11,11 @@ quim=float(input("Calificacion de quimica: "))
 
 promedio=(mat+fis+quim)/3
 
+#Esta parte obligara al codigo a hacer una pausa
+print("Procesando........")
+time.sleep(3)
+
+#Resultado
 print("La nota promedio del alumno: ",alumno, "es: ",round(promedio,2))
 
 #
