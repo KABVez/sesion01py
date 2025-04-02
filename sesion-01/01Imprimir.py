a05f3c00110fc4b7149be887de58ac848347f698
KABVez <kevinandreo56@gmail.com>
2025-04-01 20:39:07 -0500

    ad

--- sesion-01/01Imprimir.py
+++ sesion-01/01Imprimir.py
@@ -3,6 +3,17 @@
 print("hola mundo")
 print("el resultado de 5+3 es:", 5+3)
 nombre="Luis"
+#Forma 1
 print(f'Bienvenido {nombre} a nuestro skil')
+#Forma 2
 print("Bienvenido",nombre,"a nuestro curso de skill")
 #fin
+
+#Ejemplo mio
+text1=10
+text2=20
+text3=30
+
+print("resultado"
+        ,text1+text2,
+        )
